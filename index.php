b1fdb516028274f4c7f4c92ece6a4becc0dedbbe
ginoskurtloven <ginoskurtloven@gmail.com>
2016-07-13 20:50:58 +0800

    Added the Home page

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="refresh" content="30"> <!-- Refresh the document every 30 sec. -->
+		<title>Cart - Simple Ecommerce Project Site</title>
+		<meta name="description" content="Simple Website Project for small ecommerce business">
+		<meta name="author" content="Kurt Loven Ginos">
+		<meta name="keywords" content="">
+
+		<!-- Mobile Specific Metas -->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Font
+		–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+
+		<!-- CSS
+		–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+		<link rel="stylesheet" href="css/normalize.css">
+		<link rel="stylesheet" href="css/skeleton.css">
+
+
+		<!-- Favicon
+		–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+		<link rel="icon" type="img/png" href="">
+
+	</head>
+	<body>
+
+		<div class="container">
+			<div class="row">
+				<div class="twelve column">
+					<h1>Welcome to Cart 1.0</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, fugit dolorum possimus ipsa quidem inventore odit itaque praesentium ab tempore, neque repellendus ipsam sequi ratione. Ipsum cupiditate aperiam doloremque distinctio.</p>
+				</div> 
+			</div>
+		</div> <!-- end of container -->
+
+		<div class="container">
+			<div class="row">
+				<div class="four columns">
+					<h3>Sidebar</h3>
+				</div>
+				<div class="eight columns">
+					<h4>Product List</h4>
+				</div>
+			</div>
+		</div>
+		
+	</body>
+</html>
